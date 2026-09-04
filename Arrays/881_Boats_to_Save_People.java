216b4d77aae84e55e9bc4274fae9ea90546cfa1a
VanshDhattarwal <vanshjaat277@gmail.com>
2026-09-04 07:10:50 +0530

    feat: solve Boats to Save People using two pointers

---
+++ Arrays/881_Boats_to_Save_People.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+
+        Arrays.sort(people);
+
+        int left = 0;
+        int right = people.length-1;
+        int boat = 0;
+
+        while(left<right){
+            if(people[right] + people[left] <= limit){
+                boat++;
+                left++;
+                right--;
+            }else{
+                right--;
+                boat++;
+                
+            }
+        }
+
+        
+            if(right == left){
+                boat++;
+            }
+        return boat;
+        
+    }
+}
